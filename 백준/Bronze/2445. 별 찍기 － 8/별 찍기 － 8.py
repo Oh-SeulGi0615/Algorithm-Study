bd6c22cbf781c0572000ad1c81a61095f02922c0
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-07-19 19:18:13 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(1, n+1):
+    print(('*' * i) + (' ' * (n - i) * 2) + ('*' * i))
+for j in range(n-1, 0, -1):
+    print(('*' * j) + (' ' * (n - j) * 2) + ('*' * j))
