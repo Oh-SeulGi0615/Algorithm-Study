716ae0dbb07893752d381d802269239ca138a1d6
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-07-19 20:13:32 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(3):
+    arr = list(map(int, input().split()))
+
+    if arr.count(0) == 1:
+        print('A')
+    elif arr.count(0) == 2:
+        print('B')
+    elif arr.count(0) == 3:
+        print('C')
+    elif arr.count(0) == 4:
+        print('D')
+    else:
+        print('E')
