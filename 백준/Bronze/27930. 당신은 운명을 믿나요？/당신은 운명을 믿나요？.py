cde8720166e03d2117b1413bb56be87e78ebaf6c
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-07-23 23:42:15 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 48 ms, Memory: 35652 KB -BaekjoonHub

--- 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
@@ -3,20 +3,17 @@ input = sys.stdin.readline
 
 a = list(input().rstrip())
 
-list1, list2 = ['K','O','R','E','A'], ['Y','O','N','S','E','I']
-cnt1, cnt2 = 0, 0
+k, y = ['K','O','R','E','A'], ['Y','O','N','S','E','I']
 
 for i in range(len(a)):
-    if a[i] == list1[cnt1] or a[i] == list2[cnt2]:
-        if a[i] == list1[cnt1]:
-            cnt1 += 1
-        elif a[i] == list2[cnt2]:
-            cnt2 += 1
-
-    if cnt1 == len(list1) or cnt2 == len(list2):
-        if cnt1 == len(list1):
+    if a[i] == k[0]:
+        k.remove(a[i])
+        if len(k) == 0:
             print('KOREA')
             break
-        elif cnt2 == len(list2):
+        
+    elif a[i] == y[0]:
+        y.remove(a[i])
+        if len(y) == 0:
             print('YONSEI')
             break
