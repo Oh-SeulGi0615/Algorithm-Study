95a4b657c32842f795f2e4850394addc4611a79b
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-07-28 20:36:51 +0900

    [Bronze III] Title: 완전 제곱수, Time: 68 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+cnt = 0
+for b in range(1, 500):
+    for a in range(b+1, 501):
+        if a**2 - b**2 == n:
+            cnt += 1
+
+print(cnt)
