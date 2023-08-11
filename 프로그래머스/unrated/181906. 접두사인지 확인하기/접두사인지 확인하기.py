516ab1e08480402e331dcc70c4db3cb002ae97c4
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-11 13:55:25 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,9 @@
+def solution(my_string, is_prefix):
+    arr = []
+    for i in range(1, len(my_string)):
+        arr.append(my_string[:i])
+    
+    if is_prefix in arr:
+        return 1
+    else:
+        return 0
