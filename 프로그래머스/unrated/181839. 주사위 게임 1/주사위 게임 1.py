4c5e4442c0e8bead0749b115f4fac870c1174150
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-17 08:38:32 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    if a % 2 == 1 and b % 2 == 1:
+        answer = a**2 + b**2
+        return answer
+    elif a % 2 == 1 or b % 2 == 1:
+        answer = 2 * (a + b)
+        return answer
+    else:
+        answer = abs(a - b)
+        return answer
