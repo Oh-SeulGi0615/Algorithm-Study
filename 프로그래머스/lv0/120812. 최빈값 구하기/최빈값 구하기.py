4e355e57c9133fcc727a187f85a319becacfaa06
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-18 16:02:49 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.47 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,11 @@
+def solution(array):
+    count = {i:0 for i in range(1000)}
+    for j in array:
+        count[j] += 1
+
+    count = sorted(count.items(), key=lambda x: x[1], reverse=True)
+
+    if count[0][1] == count[1][1]:
+        return -1
+    else:
+        return count[0][0]
