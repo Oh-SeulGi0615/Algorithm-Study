e4fabd9cf8bd1060afd37cea1d2077808f5ca23a
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-18 13:17:11 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    a_ = list(a)
+    b_ = list(b)
+
+    cnt = 0
+    for _ in range(len(a_)):
+        if a_ == b_:
+            return cnt
+
+        a_.insert(0, a_[-1])
+        a_.pop()
+        cnt += 1
+    return -1
