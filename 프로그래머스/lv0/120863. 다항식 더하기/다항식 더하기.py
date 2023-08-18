b6206c4859f6aa16c1abab74025c04b006a8a7e1
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-18 16:23:46 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,31 @@
+def solution(polynomial):
+    arr = list(polynomial.split())
+
+    cnt = 0
+    number = 0
+    for i in arr:
+        if 'x' in i:
+            if len(i) > 1:
+                a = i[:-1]
+                cnt += int(a)
+            else:
+                cnt += 1
+        elif i == '+':
+            pass
+        else:
+            number += int(i)
+
+    if cnt > 0 and number > 0:
+        if cnt == 1:
+            return f'x + {number}'
+        else:
+            return f'{cnt}x + {number}'
+    elif cnt > 0 and number == 0:
+        if cnt == 1:
+            return f'x'
+        else:
+            return f'{cnt}x'
+    elif cnt == 0 and number > 0:
+        return f'{number}'
+    else:
+        return '0'
