17f94c871c9e9d08dff55fcb1e8f045ed71f6e9e
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-18 19:08:04 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,16 @@
+def solution(a, b):
+    import math
+
+    gcd = math.gcd(a, b)
+
+    c = int(a / gcd)
+    d = int(b / gcd)
+
+    while d > 1:
+        if d % 2 == 0:
+            d = int(d / 2)
+        elif d % 5 == 0:
+            d = int(d / 5)
+        else:
+            return 2
+    return 1
