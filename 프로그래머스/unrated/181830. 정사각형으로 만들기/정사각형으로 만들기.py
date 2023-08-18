29660fc3dddd72614f1e3793d4d55c28f637dc53
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-18 10:41:06 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    x = len(arr[0])
+    y = len(arr)
+
+    if x == y:
+        return arr
+    elif x < y:
+        for _ in range(y-x):    
+            for i in range(len(arr)):
+                arr[i].append(0)
+    elif x > y:
+        for _ in range(x-y):
+            arr.append([0] * len(arr[0]))
+    
+    return arr
