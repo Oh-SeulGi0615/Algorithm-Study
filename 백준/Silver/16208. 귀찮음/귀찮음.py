92ebfaf100094702b3094d985bc0dc7d36a454f0
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-28 09:34:04 +0900

    [Silver V] Title: 귀찮음, Time: 128 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+answer = 0
+while len(arr) > 2:
+    if arr[0] > arr[-1]:
+        answer += sum(arr[:-1]) * arr[-1]
+        arr.pop()
+    else:
+        answer += sum(arr[1:]) * arr[0]
+        arr.pop(0)
+
+answer += arr[0] * arr[1]
+print(answer)
