d13e9e0a703ec13d84b193a1229cd19c6c475c5a
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-29 08:41:19 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    print('@'*n + ' '*3*n + '@'*n)
+for _ in range(n):
+    print('@'*5*n)
+for _ in range(n):
+    print('@'*n + ' '*3*n + '@'*n)
+for _ in range(n):
+    print('@'*5*n)
+for _ in range(n):
+    print('@'*n + ' '*3*n + '@'*n)
