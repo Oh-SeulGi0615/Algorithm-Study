2c88db60ae2be34283a6704284fec055bbe751f2
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-08-29 10:18:00 +0900

    [Bronze III] Title: 헬멧과 조끼, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15781. 헬멧과 조끼/헬멧과 조끼.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+print(max(arr1) + max(arr2))
