41976933c5c4463b5a0ac0d02c50bb98b2864b40
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-09-07 09:05:00 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.py
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.py
@@ -4,13 +4,9 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-try:
+if n == 0:
+    print('divide by zero')
+else:
     avg = sum(arr) / len(arr)
     exv = [i/len(arr) for i in arr]
-
-    if n == 0 or sum(exv) == 0:
-        print('divide by zero')
-    else:
-        print(f'{avg / sum(exv):.2f}')
-except:
-    print('divide by zero')
+    print(f'{avg / sum(exv):.2f}')
