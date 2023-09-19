c70fe4477a9e10df36672382c61abc0bfb3694ab
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-09-20 08:38:11 +0900

    [Bronze III] Title: 겨울왕국 티켓 예매, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/18247. 겨울왕국 티켓 예매/겨울왕국 티켓 예매.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+
+    if n < 12 or m < 4:
+        print(-1)
+    else:
+        print(11*m + 4)
