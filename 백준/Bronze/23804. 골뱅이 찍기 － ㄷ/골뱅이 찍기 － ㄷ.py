edaadadce99064b9980c6832b58555048bacf122
Oh-SulGi <oseulgi89@gmail.com>
2023-11-10 09:40:40 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+    print('@@@@@' * n)
+for i in range(n):
+    print('@' * n)
+    print('@' * n)
+    print('@' * n)
+for i in range(n):
+    print('@@@@@' * n)
