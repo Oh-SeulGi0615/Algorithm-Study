d3b7686cb16320cac9d4ee933fa96640fd7b43fd
Oh-SulGi <oseulgi89@gmail.com>
2023-11-17 11:00:37 +0900

    [Bronze III] Title: 찬반투표, Time: 40 ms, Memory: 31120 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+if 0 in arr:
+    if arr.count(0) > int(len(arr) / 2):
+        print('INVALID')
+    elif arr.count(1) > arr.count(-1):
+        print('APPROVED')
+    elif arr.count(1) <= arr.count(-1):
+        print('REJECTED')
+else:
+    if arr.count(1) > arr.count(-1):
+        print('APPROVED')
+    else:
+        print('REJECTED')
