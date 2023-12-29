e38ce7da689f2a855da48891a80ba297bbb1da2f
Oh-SulGi <oseulgi89@gmail.com>
2023-12-29 17:23:27 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+from itertools import combinations
+
+arr = [int(input()) for _ in range(9)]
+
+arr2 = list(combinations(arr, 7))
+
+for i in arr2:
+    if sum(i) == 100:
+        arr3 = list(i)
+        for j in arr3:
+            print(j)
