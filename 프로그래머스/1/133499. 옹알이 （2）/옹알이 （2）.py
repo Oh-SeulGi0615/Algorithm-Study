959428a843a5e8a65bfff285c1967f8115e8d896
Oh-SulGi <oseulgi89@gmail.com>
2024-03-28 10:50:40 +0900

    [level 1] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -2,22 +2,11 @@ def solution(babbling):
     can_speak = ['aya', 'ye', 'woo', 'ma']
     cnt = 0
     
-    for word in babbling:
-        compare = ''
-        temp = ''
-        
-        for char in word:
-            compare += char
-            
-            if compare == temp:
-                break
-            
-            if compare in can_speak:
-                temp = compare
-                compare = ''
-        
-        if compare == '':
+    for i in babbling:
+        for j in can_speak:
+            if j*2 not in i:
+                i = i.replace(j,' ')
+        if len(i.strip()) == 0:
             cnt += 1
-    
     return cnt
                 
