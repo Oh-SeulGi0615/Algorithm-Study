54c90bf14a90b8421c4c108b7591e261305fd242
Oh-SulGi <oseulgi89@gmail.com>
2024-06-11 11:52:24 +0900

    [Bronze III] Title: Vending Machine, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/17284. Vending Machine/Vending Machine.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+
+money = 5000
+for i in arr:
+    if i == 1:
+        money -= 500
+    elif i == 2:
+        money -= 800
+    else:
+        money -= 1000
+
+print(money)
