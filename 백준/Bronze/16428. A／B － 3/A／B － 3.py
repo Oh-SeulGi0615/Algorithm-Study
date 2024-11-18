529a4569c06a06c61a1bc2b32b51e7a3c6348969
Oh-SulGi <oseulgi89@gmail.com>
2024-11-18 14:02:46 +0900

    [Bronze III] Title: A/B - 3, Time: 44 ms, Memory: 31120 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.py
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.py
@@ -1,4 +1,20 @@
 a, b = map(int, input().split())
 
-print(a//b)
-print(a%b)
+div = 0
+mod = 0
+
+if a > 0 and b > 0:
+    div = a // b
+    mod = a % b
+elif a < 0 and b > 0:
+    div = a // b
+    mod = a % b
+elif a > 0 and b < 0:
+    div = -1 * (abs(a) // abs(b))
+    mod = abs(a) % abs(b)
+elif a < 0 and b < 0:
+    div = -1 * (a // abs(b))
+    mod = a - (b * div)
+
+print(div)
+print(mod)
