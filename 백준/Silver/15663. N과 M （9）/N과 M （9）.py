b6d768492d041a591a24b5285c7eb585dd93ee27
Oh-SeulGi0615 <oseulgi89@gmail.com>
2024-12-09 12:20:24 +0900

    [Silver II] Title: N과 M (9), Time: 756 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+cnt = {i: arr.count(i) for i in arr}
+answer, saved = [], {}
+
+def back():
+  if len(answer) == m:
+    string = ' '.join(map(str, answer))
+    if string not in saved:
+      print(string)
+      saved[string] = False
+    return 
+
+  for i in arr:
+    if answer.count(i) < cnt[i]:
+      answer.append(i)
+      back()
+      answer.pop()
+
+back()
