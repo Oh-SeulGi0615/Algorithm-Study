3ed3c99c2dfb7e432175bf996b6615f4e794f1fb
Oh-SeulGi0615 <oseulgi89@gmail.com>
2025-03-06 12:27:45 +0900

    [Bronze I] Title: 무알콜 칵테일, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.py
+++ 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.py
@@ -1,9 +1,18 @@
 a, b, c = map(int, input().split())
 i, j, k = map(int, input().split())
 
-ratio = min(a/i, b/j, c/k)
-a -= i * ratio
-b -= j * ratio
-c -= k * ratio
+p, q, r = 0, 0, 0
+if min(a/i, b/j, c/k) == a/i:
+  p = a - (i * (a/i))
+  q = b - (j * (a/i))
+  r = c - (k * (a/i))
+elif min(a/i, b/j, c/k) == b/j:
+  p = a - (i * b/j)
+  q = b - (j * b/j)
+  r = c - (k * b/j)
+else:
+  p = a - (i * c/k)
+  q = b - (j * c/k)
+  r = c - (k * c/k)
 
-print(a, b, c)
+print(p, q, r)
