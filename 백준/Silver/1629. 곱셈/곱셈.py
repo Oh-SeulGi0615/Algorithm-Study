ec8e1c3ecbdc681a9c4a1272d74d822340a3eb41
Oh-SeulGi0615 <oseulgi89@gmail.com>
2025-11-17 17:03:28 +0900

    [Silver I] Title: 곱셈, Time: 88 ms, Memory: 236388 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,6 +1,17 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
 
 a, b, c = map(int, input().split())
 
-print(pow(a, b, c))
+def power(a, n, mod):
+  if n == 0:
+    return 1
+  x = power(a, n//2, mod)
+
+  if n % 2 == 0:
+    return (x * x) % mod
+  else:
+    return (x * x * a) % mod
+  
+print(power(a, b, c))
