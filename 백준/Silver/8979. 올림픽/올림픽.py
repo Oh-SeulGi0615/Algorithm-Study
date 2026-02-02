fd112975350c397d3bf846ae80f0bf16793ff295
Oh-SeulGi0615 <oseulgi89@gmail.com>
2026-02-02 15:37:50 +0900

    [Silver V] Title: 올림픽, Time: 92 ms, Memory: 109544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,17 +9,20 @@ arr = sorted(arr, key=lambda x: (-x[1], -x[2], -x[3]))
 rank = 1
 person = 0
 state = [0, 0, 0]
+dict_ = {(i+1):0 for i in range(n)}
 for i in range(n):
   if i == 0:
     person += 1
     state = arr[i][1:]
+
   elif arr[i][1:] != state:
     rank += person
     person = 1
     state = arr[i][1:]
+
   else:
     person += 1
   
-  if i == (k-1):
-    print(rank)
-    break
+  dict_[arr[i][0]] = rank
+  
+print(dict_[k])
