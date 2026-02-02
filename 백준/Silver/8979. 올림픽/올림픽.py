25a88bf71cf174c70831eeb7e2838650885ffcdc
Oh-SeulGi0615 <oseulgi89@gmail.com>
2026-02-02 15:26:19 +0900

    [Silver V] Title: 올림픽, Time: 92 ms, Memory: 108384 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+arr = sorted(arr, key=lambda x: (-x[1], -x[2], -x[3]))
+
+rank = 1
+person = 0
+state = [0, 0, 0]
+for i in range(n):
+  if i == 0:
+    person += 1
+    state = arr[i][1:]
+  elif arr[i][1:] != state:
+    rank += person
+    person = 1
+    state = arr[i][1:]
+  else:
+    person += 1
+  
+  if i == (k-1):
+    print(rank)
+    break
